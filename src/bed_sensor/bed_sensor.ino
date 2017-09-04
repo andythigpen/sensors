d4542882a51a3b18d1425846184be077c65748e5
Andrew Thigpen <andyt05@gmail.com>
2017-09-04 14:28:46 -0500

    Use a running average and peak detection.
    Calculate a running average and use peak detection in an attempt to
    eliminate false positives.  Utilizes a noise band that minimally affects
    the running average.  Values outside of the noise band have a greater
    effect on the running average.  Finally, values outside of 2x the noise
    band are completely ignored, as they're assumed to be anomalous.
    
    Also:
    
    * Reduces the number of pads (2 broke).
    * Adds a trigger limit so that at least 2 pads must be active for
      presence detection.
    * Longer calibration on startup w/ a greater initial delay before
      performing calibration.

--- src/bed_sensor/bed_sensor.ino
+++ src/bed_sensor/bed_sensor.ino
@@ -14,25 +14,32 @@
 #include <MySensor.h>
 #include "Adafruit_MPR121.h"
 
-#define SENSOR_VERSION  "0.1"
+#define SENSOR_VERSION  "0.2"
 #define DEBUG           1
 #define CHILD_ID        1
 
 #define MPR121_ADDR     0x5A    // I2C address
-#define NUM_PADS        6
-#define THRESHOLD       6
-#define COUNT_LIMIT     5       // consecutive times the sample must be above/below threshold
+#define NUM_PADS        4
 #define SAMPLE_INTERVAL 1000    // ms to update values from MPR121
+#define TRIGGER_LIMIT   2       // num of pads that must be triggered to count
 #define UPDATE_TIMES    30      // if state hasn't changed send updates after this count
 
-// MPR pad state
-uint8_t previous[NUM_PADS];
-uint8_t baseline[NUM_PADS];
-//uint8_t counter[NUM_PADS] = {0};
+
+// values are specific to the sensors and were determined through experimentation
+const float noise[NUM_PADS] = {4.0, 4.0, 2.5, 2.5};
+const float lwm[NUM_PADS] = {3.4, 5.0, 2.5, 1.7};
+const float hwm[NUM_PADS] = {1.5, 2.5, 1.0, 0.6};
+float baseline[NUM_PADS];
+float avg[NUM_PADS];
+float peak[NUM_PADS];
 uint8_t occupied[NUM_PADS] = {0};
-uint8_t states[COUNT_LIMIT] = {0};
+uint8_t totalTriggered = 0;
 uint8_t bedOccupied = 0;
 uint8_t updateCounter = 0;
+
+#define FAST_ALPHA       0.7
+#define SLOW_ALPHA       0.1
+
 
 // MySensors
 MySensor gw;
@@ -40,15 +47,46 @@ MyMessage msg(CHILD_ID, V_TRIPPED);
 
 Adafruit_MPR121 mpr121 = Adafruit_MPR121();
 
+#if DEBUG
+#define LOG_LN_DEBUG(x)  Serial.println(x)
+#define LOG_DEBUG(x)  Serial.print(x)
+#else
+#define LOG_LN_DEBUG(x)
+#define LOG_DEBUG(x)
+#endif
+
+
+
+float runningAvg(float current, float prev, float alpha) {
+    return alpha * current + (1.0 - alpha) * prev;
+}
+
+
+void calibrate() {
+    gw.sleep(5000);
+    for (uint8_t cnt = 0; cnt < 10; ++cnt) {
+        for (uint8_t i = 0; i < NUM_PADS; ++i) {
+            uint8_t current = mpr121.filteredData(i);
+            if (cnt == 0)
+                baseline[i] = (float)current;
+            else
+                baseline[i] = runningAvg(current, baseline[i], FAST_ALPHA);
+
+            avg[i] = baseline[i];
+            LOG_DEBUG(baseline[i]); LOG_DEBUG("\t");
+        }
+        LOG_LN_DEBUG();
+        gw.sleep(1000);
+    }
+}
+
 
 void setup() {
     gw.begin(NULL, 16);
     gw.sendSketchInfo("Bed Sensor", SENSOR_VERSION);
     gw.present(CHILD_ID, S_MOTION);
 
-#if DEBUG
-    Serial.println("Bed occupancy sensor");
-#endif
+    LOG_LN_DEBUG("Bed occupancy sensor");
 
     if (!mpr121.begin(MPR121_ADDR)) {
         Serial.println("MPR121 not found");
@@ -56,55 +94,63 @@ void setup() {
     }
 
     gw.sleep(1000);
-
-    for (uint8_t i = 0; i < NUM_PADS; ++i) {
-        previous[i] = baseline[i] = mpr121.filteredData(i);
-    }
-
     gw.send(msg.set(0));
 
-    Serial.println("Ready.");
+    LOG_LN_DEBUG("Calibrating...");
+    calibrate();
+
+    LOG_LN_DEBUG("Ready.");
 }
 
 
 void loop() {
-    uint8_t totalTriggered = 0;
     for (uint8_t i = 0; i < NUM_PADS; ++i) {
         uint8_t current = mpr121.filteredData(i);
+        float alpha = 0.0;
 
-        if (previous[i] > current
-                && previous[i] - current >= THRESHOLD
-                && !occupied[i]) {
-            baseline[i] = previous[i];
+        if ((current > baseline[i] + noise[i]) ||
+                (current < baseline[i] - noise[i])) {
+            // ignore values > 2 times our noise band
+            if (abs(baseline[i] - current) < 2 * noise[i])
+                alpha = FAST_ALPHA;
+        }
+        else
+            alpha = SLOW_ALPHA;
+
+        // running average of sensor value
+        avg[i] = runningAvg(current, avg[i], alpha);
+
+        // peak detection
+        if (avg[i] > peak[i])
+            peak[i] = avg[i];
+
+        // presence detection
+        if (!occupied[i] && avg[i] < baseline[i] - lwm[i]) {
             occupied[i] = 1;
+            totalTriggered += 1;
+            baseline[i] = peak[i];
         }
-        else {
-            occupied[i] = current <= baseline[i] - THRESHOLD;
+        else if (occupied[i] && avg[i] > baseline[i] - hwm[i]) {
+            occupied[i] = 0;
+            totalTriggered -= 1;
+            peak[i] = 0;
         }
 
-        totalTriggered += occupied[i];
-        previous[i] = current;
-
-#if DEBUG
-        Serial.print(current); Serial.print("\t");
-        Serial.print(baseline[i]); Serial.print("\t");
-        Serial.print(occupied[i]); Serial.print("\t");
-#endif
+        LOG_DEBUG(current); LOG_DEBUG("\t");
+        LOG_DEBUG(avg[i]); LOG_DEBUG("\t");
+        LOG_DEBUG(baseline[i]); LOG_DEBUG("\t");
+        LOG_DEBUG(peak[i]); LOG_DEBUG("\t");
+        LOG_DEBUG(occupied[i]); LOG_DEBUG("\t");
     }
 
-    for (uint8_t i = 0; i < COUNT_LIMIT - 1; ++i) {
-        states[i] = states[i + 1];
-    }
-    states[COUNT_LIMIT - 1] = totalTriggered >= 1 ? 1 : 0;
-
-    if (memchr(states, 0, COUNT_LIMIT) == NULL) {
+    if (totalTriggered >= TRIGGER_LIMIT) {
         if (!bedOccupied || updateCounter++ > UPDATE_TIMES) {
             gw.send(msg.set(1));
             updateCounter = 0;
         }
         bedOccupied = 1;
     }
-    else if (memchr(states, 1, COUNT_LIMIT) == NULL) {
+    else {
         if (bedOccupied || updateCounter++ > UPDATE_TIMES) {
             gw.send(msg.set(0));
             updateCounter = 0;
@@ -112,10 +158,7 @@ void loop() {
         bedOccupied = 0;
     }
 
-#if DEBUG
-    Serial.print(bedOccupied);
-    Serial.println();
-#endif
+    LOG_LN_DEBUG();
 
     gw.sleep(SAMPLE_INTERVAL);
 }
